139abca87570c85ed136a3eb0a8620e044ddbbec
Amal <amalsabuesthaphanos@gmail.com>
2026-07-15 15:29:20 +0530

    Update largest.py

--- Array/largest.py
+++ Array/largest.py
@@ -1,1 +1,12 @@
 
+arr = [12, 35, 1, 10, 34, 1]
+l=len(arr)
+largest=0
+for i in range(l):
+        if arr[i]>largest:
+            largest=arr[i]
+print(largest)
+
+        
+#2,1,4,3,5
+#13, 46, 24, 52, 20, 9
