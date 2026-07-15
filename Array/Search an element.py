5870737c98fff360f477d82221cc1a4d01264ced
Amal <amalsabuesthaphanos@gmail.com>
2026-07-15 15:28:32 +0530

    Find the largest element in an array

--- Array/Search an element.py
+++ Array/Search an element.py
@@ -1,1 +1,13 @@
 
+arr = [12, 35, 1, 10, 34, 1]
+l=len(arr)
+largest=0
+for i in range(l):
+        if arr[i]>largest:
+            largest=arr[i]
+print(largest)
+
+        
+#2,1,4,3,5
+#13, 46, 24, 52, 20, 9
+#10,5,10
